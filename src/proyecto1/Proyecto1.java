2add0091f1893c51e69ced00dcc90065ec966377
Ivn123 <ivnrdz085@gmail.com>
2022-09-14 21:07:57 -0500

    se agrega el proyecto

---
+++ src/proyecto1/Proyecto1.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto1;
+
+
+public class Proyecto1 {
+
+  
+    public static void main(String[] args) {
+     int x=5;
+     int y=6;
+     int suma=x+y;
+     System.out.print("El resultado de la suma: "+suma);
+    }
+   
+    
+}
